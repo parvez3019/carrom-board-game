8aa4eff094d36f35302eebd872690b2001fcc9d1
Parvez Hassan <pha3019@gmail.com>
2020-03-06 21:25:57 +0530

    Fix tests

--- cleanStrike/game_test.go
+++ cleanStrike/game_test.go
@@ -19,8 +19,9 @@ func setUpGame(blackCoins int, redCoins int) (*Game, *player.Player, *player.Pla
 func TestCleanStrike_ShouldReturnGameAsDrawWithScore2and0As0_WhenPlayer1StrikeTwiceInRow(t *testing.T) {
 	game, player1, player2 := setUpGame(2, 0)
 
-	game.Move(player1, STRIKE, "")
-	game.Move(player1, STRIKE, "")
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKE, "")
+	game.Move(STRIKE, "")
 
 	assert.Equal(t, player1.Score(), 2)
 	assert.Equal(t, player2.Score(), 0)
@@ -30,17 +31,29 @@ func TestCleanStrike_ShouldReturnGameAsDrawWithScore2and0As0_WhenPlayer1StrikeTw
 func TestCleanStrike_ShouldReturnAsDrawEvenWith3PlusLeadForOnePlaceIfLacksByMin5Constraint_WhenPlayer1score1AndPlayer2Score4(t *testing.T) {
 	game, player1, player2 := setUpGame(9, 1)
 
-	game.Move(player1, STRIKE, "") // P1: 1
-	game.Move(player2, STRIKE, "") // P2: 1
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKE, "") // P1: 1
 
-	game.Move(player1, MULTISTRIKE, "") // P1: 3
-	game.Move(player2, REDSTRIKE, "")   // P2: 4
+	game.SetCurrentPlayer(player2)
+	game.Move(STRIKE, "") // P2: 1
 
-	game.Move(player1, STRIKERSTRIKE, "") // P1: 2
-	game.Move(player2, MULTISTRIKE, "")   // P2: 6
+	game.SetCurrentPlayer(player1)
+	game.Move(MULTISTRIKE, "") // P1: 3
 
-	game.Move(player1, NONE, "")                  // P1: 1
-	game.Move(player2, DEFUNCTCOIN, carrom.BLACK) // P2: 4
+	game.SetCurrentPlayer(player2)
+	game.Move(REDSTRIKE, "")   // P2: 4
+
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKERSTRIKE, "")
+
+	game.SetCurrentPlayer(player2) // P1: 2
+	game.Move(MULTISTRIKE, "")   // P2: 6
+
+	game.SetCurrentPlayer(player1)
+	game.Move(NONE, "")                  // P1: 1
+
+	game.SetCurrentPlayer(player2)
+	game.Move(DEFUNCTCOIN, carrom.BLACK) // P2: 4
 
 	assert.Equal(t, player1.Score(), 1)
 	assert.Equal(t, player2.Score(), 4)
@@ -51,24 +64,39 @@ func TestCleanStrike_ShouldReturnAsDrawEvenWith3PlusLeadForOnePlaceIfLacksByMin5
 func TestCleanStrike_ShouldReturnAsDrawPlayer2AsWinner_WhenPlayer1score1AndPlayer2Score5(t *testing.T) {
 	game, player1, player2 := setUpGame(9, 1)
 
-	game.Move(player1, STRIKE, "") // P1: 1
-	game.Move(player2, STRIKE, "") // P2: 1
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKE, "") // P1: 1
 
-	game.Move(player1, MULTISTRIKE, "") // P1: 3
-	game.Move(player2, REDSTRIKE, "")   // P2: 4
+	game.SetCurrentPlayer(player2)
+	game.Move(STRIKE, "") // P2: 1
 
-	game.Move(player1, STRIKERSTRIKE, "") // P1: 2
-	game.Move(player2, MULTISTRIKE, "")   // P2: 6
+	game.SetCurrentPlayer(player1)
+	game.Move(MULTISTRIKE, "") // P1: 3
 
-	game.Move(player1, NONE, "")                  // P1: 1
-	game.Move(player2, DEFUNCTCOIN, carrom.BLACK) // P2: 4
+	game.SetCurrentPlayer(player2)
+	game.Move(REDSTRIKE, "")   // P2: 4
 
-	game.Move(player1, STRIKE, "") // P1: 2
-	game.Move(player2, STRIKE, "") // P2: 5
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKERSTRIKE, "") // P1: 2
+
+	game.SetCurrentPlayer(player2)
+	game.Move(MULTISTRIKE, "")  // P2: 6
+
+	game.SetCurrentPlayer(player1)
+	game.Move(NONE, "")      // P1: 1
+
+	game.SetCurrentPlayer(player2)
+	game.Move(DEFUNCTCOIN, carrom.BLACK) // P2: 4
+
+	game.SetCurrentPlayer(player1)
+	game.Move(STRIKE, "") // P1: 2
+
+	game.SetCurrentPlayer(player2)
+	game.Move(STRIKE, "") // P2: 5
 
 	assert.Equal(t, player1.Score(), 2)
 	assert.Equal(t, player2.Score(), 5)
-	assert.Equal(t, game.Result(player1, player2), "Player2 won the game. Final Score: 5-2")
+	assert.Equal(t, game.Result(player1, player2), "Player2 won the game. Final Score: 5 : 2")
 }
 
 func TestCleanStrike_ShouldReturnWinnerAsPlayer1_WhenPlayer1score5AndPlayer2Score0(t *testing.T) {
@@ -77,7 +105,7 @@ func TestCleanStrike_ShouldReturnWinnerAsPlayer1_WhenPlayer1score5AndPlayer2Scor
 	player1.UpdateScore(5)
 	player2.UpdateScore(0)
 
-	assert.Equal(t, game.Result(player1, player2), "Player1 won the game. Final Score: 5-0")
+	assert.Equal(t, game.Result(player1, player2), "Player1 won the game. Final Score: 5 : 0")
 }
 
 func TestCleanStrike_ShouldReturnDraw_WhenPlayer1score3AndPlayer2Score5(t *testing.T) {
